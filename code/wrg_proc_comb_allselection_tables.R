745849438a8422a674bf3cd64cd2e83a799df7ce
kimfranklin <kymmiy9@aol.com>
2020-08-14 17:36:35 +0000

    made script to read in raven selection tables for all years and combine them

---
+++ code/wrg_proc_comb_allselection_tables.R
@@ -0,0 +1,79 @@
+## wrg_proc_comb_allselection_tables.R ##
+# combing all platform (NOAA) acoustic data for all years
+
+# input -------------------------------------------------------------------
+
+# defines input directory
+idir = 'data/raw/acoustic/'
+
+# defines output directory
+odir = 'data/interim/'
+
+# defining characteristic for output file
+char = 'all_noaa_'
+
+# setup -------------------------------------------------------------------
+
+library(tidyverse)
+
+# saving output file for selection tables 
+ofile = paste0(odir, char, 'selections.rds')
+
+# process -----------------------------------------------------------------
+
+# find all years 
+flista = list.files(path = idir, pattern = '20*', full.names = TRUE)
+
+# list data files
+flist = list.files(path = paste0(flista,'/noaa/processed'), pattern = '*_selections.txt$', full.names = TRUE)
+
+# define empty list to hold selections
+DFa = vector('list',length = length(flista))
+DF = vector('list',length = length(flist))
+
+# loop through and read in selection tables
+for(ii in seq_along(flist)){
+  
+  # define input file
+  ifile = flist[ii]
+  
+  # read selection table
+  tmp = read.delim(file = ifile, sep = '\t', as.is = TRUE)
+  
+  # isolate deployment name
+  dname = gsub(pattern = '_selections.txt', replacement = '', x = basename(ifile))
+  
+  # adding column of deployment information
+  tmp$sono_id = dname
+  
+  # isolate year
+  yname = as.character(strsplit(ifile,"/")[[1]][4])
+  
+  # adding year column
+  tmp$year= yname
+  
+  # isolate platform name
+  pname = as.character(strsplit(flist,"/")[[1]][5])
+  
+  # add column with platform name
+  tmp$platform = pname
+  
+  # add selection table to list
+  DF[[ii]] = tmp
+}
+
+# combine all selection tables into one df
+df = bind_rows(DF)
+
+# editing the id column (this is to match the logs formating)
+df$id = paste0(df$year,'_', df$platform,'_', df$sono_id)
+
+# remove deployments that have no useful information
+df = df[!(df$id=="2017_noaa_DEP17"),]
+df = df[!(df$id=="2018_noaa_DEP07"),]
+df = df[!(df$id=="2018_noaa_DEP13"),]
+df = df[!(df$id=="2018_noaa_DEP14"),]
+df = df[!(df$id=="2019_noaa_DEP75b"),]
+
+# save data frame
+saveRDS(df, file = ofile)
