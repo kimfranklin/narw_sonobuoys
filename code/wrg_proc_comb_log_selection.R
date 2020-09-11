c77ae15a636c1b49b7ae8ce1c7a0194685a6376c
Kimberly Franklin <kfranklin@Kimberlys-MacBook-Pro.local>
2020-09-11 14:35:51 -0300

    commented out time stuff - not sure if will need in future

--- code/wrg_proc_comb_log_selection.R
+++ code/wrg_proc_comb_log_selection.R
@@ -20,10 +20,10 @@ ofile = 'data/processed/all_noaa_acoustic.rds'
 # fix up log
 log = log %>% 
   transmute(date = date, # this is POSIXct POSIXt
-            datetime = time, # this is character
-            time_UTC = format(as.POSIXct(`time UTC`, format= "%Y-%m-%d %H:%M:%S"),
-                              format = "%H:%M:%S"),
-            datetime_UTC = paste(date, time_UTC),
+            # datetime = time, # this is character
+            # time_UTC_correct_from_Tim_Cole_gps_records = format(as.POSIXct(`time UTC`, format= "%Y-%m-%d %H:%M:%S"),
+            #                   format = "%H:%M:%S"),
+            datetime_UTC = datetime_UTC_correct,
             lat = lat, # this is numeric
             lon = lon, # this is numeric
             yday = yday,
