528de612b6ebbfb944ce4254fe1c363a3edef4da
Kimberly Franklin <kfranklin@Kimberlys-MacBook-Pro.local>
2020-09-11 14:41:31 -0300

    moved to old folder

--- code/wrg_sightings_excel_to_rds.R
+++
@@ -1,8 +0,0 @@
-## sightings_excel_to_rds ##
-
-library(readxl)
-
-#sightings information
-sightings = read_excel('data/sightings_data.xlsx')
-saveRDS(sightings, file = 'data/sightings_data.rds')
-sightings = readRDS('data/sightings_data.rds')
