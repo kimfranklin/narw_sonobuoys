f5d46ec3f4daa3746a8f840469c7a7889f910685
Kimberly Franklin <kfranklin@Kimberlys-MacBook-Pro.local>
2020-09-22 14:56:43 -0300

    updated master to reflect 'new' scripts

--- master.R
+++ master.R
@@ -3,22 +3,21 @@
 
 # process -----------------------------------------------------------------
 
-# 1. combine all selection tables (produces `data/interim/all_noaa_selections.rds`)
-source('code/wrg_proc_comb_allselection_tables.R')
+# 1. combine all selection tables and merge sonobuoy log with selection tables
+# (produces `data/interim/all_noaa_selections.rds`and `data/processed/all_noaa_acoustic.rds`)
+source('code/wrg_acoustic.R')
 
-# 2. merge sonobuoy log with selection tables (produces `data/processed/all_noaa_acoustic.rds`)
-source('code/wrg_proc_comb_log_selection.R')
+# 2. combine all photo-id data and subset photo-id data (produces `data/interim/all_noaa_photoid_comb.rds`
+# and `data/processed/all_noaa_photoid.rds`)
+source('code/wrg_photo_id.R')
 
-# 3. combine all photo-id data (produces `data/interim/all_noaa_photoid_comb.rds`)
-source('code/wrg_comb_photoid.R')
-
-# 4. subset photo-id data (produces `data/processed/all_noaa_photoid.rds`)
-source('code/wrg_photoid.R')
-
-# 5. combine acoustic and photo-id data (produces `data/processed/proc_acou_photoid.rds`)
+# 3. combine acoustic and photo-id data (produces `data/processed/proc_acou_photoid.rds`)
 source('code/wrg_comb_acou_photoid.R')
 
 # plot --------------------------------------------------------------------
+
+# plot deployment map
+source('code/fig_map.R')
 
 # plot histogram...
 # source('code/f_histogram.R')
