7d695b19075ae3d4d4ddb5b90396d34341c7d412
Kimberly Franklin <kfranklin@Kimberlys-MacBook-Pro.local>
2020-12-17 13:45:22 -0400

    created script to extract 5 number summary results for each variable to csv format

---
+++ code/tbl_5_num_summary.R
@@ -0,0 +1,30 @@
+# 5_num_summary.R #
+
+# 5 number summary for each variable of interest in the dataframe 
+# includes: max, min, median, first quartiile, third quartile
+
+# libraries
+library(tidyverse)
+library(data.table)
+
+# read in the data
+df= readRDS("data/processed/proc_acou_photoid.rds")
+
+# subset data so only variables of interest are shown
+tmp = df %>%
+   select(up,up_per_hr,up_per_hr_per_whale,
+          gs,gs_per_hr,gs_per_hr_per_whale,
+          mf,mf_per_hr,mf_per_hr_per_whale,
+          num_sighting, ratio_male_female,
+          adult_male,adult_female,juvenile_male,juvenile_female,unknown,
+          foraging_bhv_whale,social_bhv_whale,other_bhv_whale,
+          rec_duration,dep_duration)
+
+# make matrix/array of 5 number summary stats of variables of interest
+tmps = do.call(cbind, lapply(tmp, summary))
+
+# transpose that matrix/array
+tmpss = t(tmps)
+
+# save the matrix/array
+write.csv(tmpss,"data/processed/5_num_summary.csv")
