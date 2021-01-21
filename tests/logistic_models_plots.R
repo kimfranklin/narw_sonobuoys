a78af8c2e81a24b9c4ebb64962a5764ac8f5aa96
Hansen Johnson <hansen.johnson@dal.ca>
2021-01-21 08:31:45 -0500

    fix forso_bi behavior variable and build combined plot

--- tests/logistic_models_plots.R
+++ tests/logistic_models_plots.R
@@ -9,20 +9,22 @@ library(ggpubr)
 # data
 df = readRDS("data/processed/proc_acou_photoid.rds")
 
+# dominant behaviour ------------------------------------------------------
+
 # data set up
 # add new column for estimating probabilty of foraging or socalizing
 # forso_bi
 df$forso = df$social_bhv_whale/df$foraging_bhv_whale
-df[,"forso_bi"] <- 0
-df$forso_bi[df$forso > 1] <- 1
-df$forso_bi[df$forso == 1] <- 0.5
+df$forso_bi[df$forso > 1 | is.infinite(df$forso)] = 1
+df$forso_bi[df$forso < 1] = 0
+df = drop_na(df, forso_bi)
 
 # plots
 # foraging upcall
-sightm = glm(foraging_bi ~ up_per_hr, data = df, family = 'binomial')
+sightm = glm(forso_bi ~ up_per_hr, data = df, family = 'binomial')
 summary(sightm)
 
-aplt = ggplot(df, aes(x=up_per_hr, y=foraging_bi)) +
+aplt = ggplot(df, aes(x=up_per_hr, y=forso_bi)) +
   geom_point() +
   geom_smooth(method = "glm", 
               method.args = list(family = "binomial"), 
@@ -30,10 +32,10 @@ aplt = ggplot(df, aes(x=up_per_hr, y=foraging_bi)) +
 aplt
 
 # foraging gunshot
-sightm = glm(foraging_bi ~ gs_per_hr, data = df, family = 'binomial')
+sightm = glm(forso_bi ~ gs_per_hr, data = df, family = 'binomial')
 summary(sightm)
 
-bplt = ggplot(df, aes(x=gs_per_hr, y=foraging_bi)) +
+bplt = ggplot(df, aes(x=gs_per_hr, y=forso_bi)) +
   geom_point() +
   geom_smooth(method = "glm", 
               method.args = list(family = "binomial"), 
@@ -41,15 +43,34 @@ bplt = ggplot(df, aes(x=gs_per_hr, y=foraging_bi)) +
 bplt
 
 # foraging midfreq
-sightm = glm(foraging_bi ~ mf_per_hr, data = df, family = 'binomial')
+sightm = glm(forso_bi ~ mf_per_hr, data = df, family = 'binomial')
 summary(sightm)
 
-cplt = ggplot(df, aes(x=mf_per_hr, y=foraging_bi)) +
+cplt = ggplot(df, aes(x=mf_per_hr, y=forso_bi)) +
   geom_point() +
   geom_smooth(method = "glm", 
               method.args = list(family = "binomial"), 
               se = FALSE)
 cplt
+
+# combined plot
+dfl = df %>%
+  rename(`mid-frequency` = mf_per_hr, `gunshot` = gs_per_hr, `upcall` = up_per_hr) %>%
+  pivot_longer(c(`mid-frequency`,gunshot,upcall), names_to = 'type', values_to = 'rate') %>%
+  select(type,rate,forso_bi)
+
+ggplot(dfl, aes(x=rate, y=forso_bi, group = type, color = type, fill = type)) +
+  geom_point(shape = 1) +
+  geom_smooth(method = "glm",formula = 'y ~ x',
+              method.args = list(family = "binomial"), 
+              se = TRUE) +
+  labs(x = 'Call rate (calls/h)', y = 'Probability', color = 'Call type', fill = 'Call type') +
+  facet_wrap(~type, ncol = 1) +
+  theme_bw()
+
+# HDJ STOPPED HERE #
+
+# social and foraging behaviours ------------------------------------------
 
 # social upcall
 sightm = glm(social_bi ~ up_per_hr, data = df, family = 'binomial')
