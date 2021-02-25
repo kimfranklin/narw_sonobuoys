cbc95cd26398fa63e863a84336b4f9f07604889c
Kimberly Franklin <kfranklin@Kimberlys-MacBook-Pro.local>
2021-02-25 11:47:06 -0400

    updated

--- master.R
+++ master.R
@@ -14,11 +14,40 @@ source('code/wrg_photo_id.R')
 # 3. combine acoustic and photo-id data (produces `data/processed/proc_acou_photoid.rds`)
 source('code/wrg_comb_acou_photoid.R')
 
+
+# test markdowns/scripts --------------------------------------------------
+# this section is not necessary to run unless a bunch of new data is added
+
+# these markdown files are for seeing what kind of data we have and how we want 
+# to represent it/show the trends, some of the Rmd files are testing models
+# assumptions to see if the models are valid
+source('tests/spearman_matrix_pval_matrix.Rmd')
+source('tests/glm_testing.Rmd')
+source('tests/est_call_rate_stepwise.Rmd')
+source('tests/overdis_0inf.Rmd')
+source('tests/year_comparison.Rmd')
+source('tests/est_num_whales_models.Rmd')
+source('tests/est_call_rate_models.Rmd')
+
+# these scripts are sanity checks/double checking what we are doing is valid
+source('tests/pca.R')
+source('tests/check_call_rate.R')
+
+# these are testing the time and space distributions
+source('tests/fig_target_plot_photoid.R')
+source('tests/fig_track_maps.R') # this might not work because it uses processed 
+# sightings data which is a different script not listed here (only photo id data 
+# is processed in this master script)
+
+# these maps are faceted by year and month to see in depth trends
+source('tests/fig_sono_map_facet_month_yr.R')
+source('tests/space_map_variables.R')
+
 # basic info from data set ------------------------------------------------
 
 # plot deployment map
-source('code/proc_map_gsl.R')
-source('code/fig_map_OFFICIALOFFICIAL.R')
+source('code/proc_map_data.R')
+source('code/fig_map_official_for_ms.R')
 
 # produce summary table 
 source('code/tbl_5_num_summary.R')
@@ -26,13 +55,16 @@ source('code/tbl_5_num_summary.R')
 # plot correlation matrix (this script includes the one in the app/supp)
 source('code/fig_corr_matrix_manuscript.R')
 
+# plot scatter plots
+source('code/fig_scatterplots_new.R')
+
 # statistical analyses ----------------------------------------------------
 
 # characterize whale abundance using single variable linear regressions
 source('code/tbl_est_num_sighting.R')
 
 # characterize calling rates using single variable neg. binomial regressions
-source('code/tbl_est_call_rates')
+source('code/tbl_est_call_rates.R')
 
 # characterize calling rates using stepwise multivariate neg. binomial regression
 source('code/tbl_est_call_rates_stepwise.R')
