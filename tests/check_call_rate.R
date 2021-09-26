7023b048aa7174123a6b6ab2d55a48f8cc65f913
Kimberly Franklin <kfranklin@Kimberlys-MacBook-Pro.local>
2021-09-25 21:45:41 -0300

    added KW test and fix so that saved as figure and csv file for supplementary doc

--- tests/check_call_rate.R
+++ tests/check_call_rate.R
@@ -2,6 +2,8 @@
 # quick check for change in call rate within deployment
 
 library(tidyverse)
+library(patchwork)
+library(data.table)
 
 # read and format call data
 df = readRDS('data/processed/all_noaa_acoustic.rds') %>%
@@ -26,11 +28,12 @@ cbd = df %>%
   )
 
 # plot
-ggplot()+
+p = ggplot()+
   geom_path(data = cbd, aes(x = time, y = call_n, group = id, color = yday), size = 1) +
   scale_color_viridis_c() +
   labs(x = 'Time (h)', y = 'Cumulative call count', color = 'Day of year')+
-  theme_bw()
+  theme_bw()+ 
+  theme(text = element_text(size = 15, family = "serif"))
 
 # assign calls to first or second half of each deployment
 fvs = df %>%
@@ -43,7 +46,50 @@ fvs = df %>%
   count()
 
 # plot
-ggplot()+
+g = ggplot()+
   geom_boxplot(data = fvs, aes(x = grp, y = n), fill = 'grey') +
-  labs(x = 'Call time within deployment', y = 'Call count') +
-  theme_bw()
+  labs(x = 'Deployment time available for calling', y = 'Call count') +
+  theme_bw()+ 
+  theme(text = element_text(size = 15, family = "serif"))
+
+# SAVE
+# output file 
+ofile = 'calls_firsthalf_vs_secondhalf.png'
+
+# figure directory
+fig_dir = 'figures/'
+
+stacked = wrap_plots(p,g, ncol = 1)+
+  plot_annotation(tag_levels = 'a', tag_suffix = ')')
+
+# save
+ggsave(filename = paste0(fig_dir, ofile), plot = stacked,
+       height = 9, width = 7, dpi = 300)
+
+
+
+# rank sum test to compare first half to second half 
+a = kruskal.test(n~grp, data = fvs)
+a = do.call(rbind.data.frame, a)
+a=data.table::transpose(a)
+
+# round to 3 decimal places
+a$V1 = round(as.numeric(a$V1), 3)
+a$V3 = round(as.numeric(a$V3), 3)
+
+# rename column names
+a = a %>% 
+  dplyr::rename(
+    test_stat = V1,
+    degrees_freedom = V2,
+    p_val = V3,
+    test_type = V4,
+    variables_comp = V5
+  )
+
+# save rank test results 
+# set dataframe to be a table 
+setDT(a)
+
+# save data table 
+write.csv(a,"data/processed/tbl_calls_firsthalf_vs_secondhalf.csv")
